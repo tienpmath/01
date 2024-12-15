8b0ea068b27452f99cae772363266ad16e8899f4
tienpmath <designdalat@gmail.com>
2024-12-15 10:27:02 +0700

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import { auth } from "@/auth";
 import HomePage from "@/components/layout/homepage";
 
 export default async function Home() {
-
   return (
     <div>
       <HomePage />
+      <h1>chào mừng admin</h1>
     </div>
   );
 }
