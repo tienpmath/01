c758b0b42ca2f1a200a1189dbc5dae12784ec963
tienpmath <designdalat@gmail.com>
2024-12-15 11:10:45 +0700

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { auth } from "@/auth";
 import HomePage from "@/components/layout/homepage";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <div>
       <HomePage />
       <h1>chào mừng admin</h1>
+      <Link href="/auth/login">Login</Link>
     </div>
   );
 }
